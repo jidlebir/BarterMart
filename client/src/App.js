7a7e8c96b06f76bbb6ad7bbf1bc8d425d8194bf7
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-19 20:33:54 -0700

    adding basics

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import ApolloClient from 'apollo-boost';
 import Home from './pages/Home';
 
 const client = new ApolloClient({
-  uri: '/graphql'
+  uri: '/Bartermart'
 });
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <div className="flex-column justify-flex-start min-100-vh">
         <header className="bg-secondary mb-4 py-2 flex-row align-center">
           <div className="container flex-row justify-space-between-lg justify-center align-center">
-            <h1>GraphQL Library</h1>
+            <h1>Welcome to Barter Mart</h1>
           </div>
         </header>
         <div className="container">
