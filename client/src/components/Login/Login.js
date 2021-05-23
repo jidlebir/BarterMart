cc8fabd38a8d34082bbf834225de222828756e34
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-22 17:20:40 -0700

    adding login and server.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,5 +1,35 @@
 import React from 'react';
 import './Login.css';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Login.css';
+
+export default function Login({ setToken }) {
+  return(
+    <div className="login-wrapper">
+      <h1>Please Log In</h1>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+}
 export default function Login() {
   return(
     <div className="login-wrapper">
--- server/server.js
+++ server/server.js
@@ -6,6 +6,20 @@ const path = require('path');
 const cors = require('cors');
 const { authMiddleware } = require('./utils/auth');
 app.use(cors());
+
+
+const express = require('express');
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'Peter got TECHED'
+  });
+});
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
@@ -28,8 +42,17 @@ app.get('*', (req, res) => {
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  const express = require('express');
+const cors = require('cors')
+const app = express();
+
+app.use(cors());
+
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
   });
 });
+
+app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+  });
