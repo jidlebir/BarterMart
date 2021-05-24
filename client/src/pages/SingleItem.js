b9fe65a158762ccaf3b5da4eba2279d4f81b06dc
Bravo <gmb2388@yahoo.com>
2021-05-24 03:17:53 -0700

    added singleitem page and queries

--- client/src/pages/SingleItem.js
+++ client/src/pages/SingleItem.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import CommentList from '../components/CommentList';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_ITEM } from '../utils/queries';
 
 const SingleItem = props => {
+    const { id: itemId } = useParams();
+
+    const { loading, data } = useQuery(QUERY_ITEM, {
+        variables: { id: itemId }
+    });
+    const item = data?.item || {};
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <div className="card mb-3">
                 <p className="card-header">
                     <span style={{ fontWeight: 700 }} className="text-light">
-                        Username
-          </span>{' '}
-          item on createdAt
-        </p>
+                        {item.username}
+                    </span>{' '}
+          item on {item.createdAt}
+                </p>
                 <div className="card-body">
-                    <p>Item Description</p>
+                    <p>{item.description}</p>
                 </div>
             </div>
+
+            {item.commentCount > 0 && <CommentList comments={item.comments} />}
         </div>
     );
 };
