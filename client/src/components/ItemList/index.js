07ba43572b83b4821d2d68249cc490261de2c572
Bravo <gmb2388@yahoo.com>
2021-05-24 21:03:04 -0700

    mapped cards

--- client/src/components/ItemList/index.js
+++ client/src/components/ItemList/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { render } from "react-dom";
 // import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 import ItemCard from "../Cards/product";
@@ -13,27 +14,28 @@ const ItemList = ({ items, title }) => {
       <h3>{title}</h3>
       {items &&
         items.map((item) => (
-          <div key={item._id} className="card mb-3">
-            <p className="card-header">
-              <Link
-                to={`/profile/${item.username}`}
-                style={{ fontWeight: 700 }}
-                className="text-light"
-              >
-                {item.username}
-              </Link>{" "}
-              item on {item.createdAt}
-            </p>
-            <div className="card-body">
-              <Link to={`/item/${item._id}`}>
-                <p>{item.description}</p>
-                <p className="mb-0">
-                  Comments: {item.commentCount} || Click to{" "}
-                  {item.commentCount ? "see" : "start"} the discussion!
-                </p>
-              </Link>
-            </div>
-          </div>
+          <ItemCard item={item} />
+          // <div key={item._id} className="card mb-3">
+          //   <p className="card-header">
+          //     <Link
+          //       to={`/profile/${item.username}`}
+          //       style={{ fontWeight: 700 }}
+          //       className="text-light"
+          //     >
+          //       {item.username}
+          //     </Link>{" "}
+          //     item on {item.createdAt}
+          //   </p>
+          //   <div className="card-body">
+          //     <Link to={`/item/${item._id}`}>
+          //       <p>{item.description}</p>
+          //       {/* <p className="mb-0">
+          //         Comments: {item.commentCount} || Click to{" "}
+          //         {item.commentCount ? "see" : "start"} the discussion!
+          //       </p> */}
+          //     </Link>
+          //   </div>
+          // </div>
         ))}
     </div>
   );
