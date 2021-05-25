9fb0261d0a8469c3101b5f0f825bc15b37e3e638
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-24 22:25:29 -0700

    updated

--- client/src/pages/Homepages.js
+++ client/src/pages/Homepages.js
@@ -25,65 +25,22 @@ import { makeStyles } from "@material-ui/core/styles";
 import Auth from "../utils/auth";
 import ItemList from '../components/ItemList';
 
-<<<<<<< HEAD
-
-import Auth from '../utils/auth';
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_ITEMS} from '../utils/queries';
-import { LOGIN_USER } from '../utils/mutations';
-
-
-const Homepages = () => {
-  const { loading, data } = useQuery(QUERY_ITEMS);
-<<<<<<< HEAD
-  const { data: userData } = useQuery(LOGIN_USER);
-  const thoughts = data?.thoughts || [];
-=======
-=======
 const Homepages = () => {
   const { loading, data } = useQuery(QUERY_ITEMS);
   console.log(data)
->>>>>>> 07ba43572b83b4821d2d68249cc490261de2c572
   // const { data: userData } = useQuery(QUERY_ACCOUNT_BASIC);
   const items = data?.items || [];
->>>>>>> 17c9f8cea30f79c6179d0d3fb8abf92f44b083e0
 
   // const loggedIn = Auth.loggedIn();
 
   return (
     <main>
-<<<<<<< HEAD
-      <div className="flex-row justify-space-between">
-        {loggedIn && (
-          <div className="col-12 mb-3">
-            <Profile/>
-          </div>
-        )}
-        <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <QUERY_ITEMS ={thoughts} title="Any good Barters?(s)..." />
-          )}
-        </div>
-        {loggedIn && userData ? (
-          <div className="col-12 col-lg-3 mb-3">
-            <singleitems
-              username={userData.me.username}
-              friendCount={userData.me.friendCount}
-              friends={userData.me.friends}
-            />
-          </div>
-        ) : null}
-      </div>
-=======
-
+      
       <h1>Dashboard</h1>
       <ItemList items={items} />
 
->>>>>>> 17c9f8cea30f79c6179d0d3fb8abf92f44b083e0
     </main>
   );
 };
 
-export default Home;
+export default Homepages;
