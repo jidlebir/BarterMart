b07877acd3a8c5f2e43d7219b6450a875e2359bf
Abigail Adams <abigail-adams@gmail.com>
2021-05-25 00:46:45 -0700

    merging with main

--- client/src/pages/Homepages.js
+++ client/src/pages/Homepages.js
@@ -7,7 +7,13 @@ import { QUERY_ITEMS } from "../utils/queries";
 import { makeStyles } from "@material-ui/core/styles";
 
 // import clsx from "clsx";
-// import Card from "@material-ui/core/Card";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  CardHeader,
+} from "@material-ui/core/";
 // import CardHeader from "@material-ui/core/CardHeader";
 // import CardMedia from "@material-ui/core/CardMedia";
 // import CardContent from "@material-ui/core/CardContent";
