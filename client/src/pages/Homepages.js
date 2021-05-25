8b7df6133cba6427daa11b1d3339994fe8a466dd
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-05-24 19:21:26 -0700

    adding to homepage

--- client/src/pages/Homepages.js
+++ client/src/pages/Homepages.js
@@ -1,43 +1,50 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_ITEMS } from '../utils/queries';
-
-
-
+import Login from '../components/Login';
+import NoMatch from '../components/NoMatch';
+import Profile from '../components/Profile';
+import Signup from  '../components/Signup';
+import singleitems from '../pages/SingleItem';
 
 
 import Auth from '../utils/auth';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_ITEMS} from '../utils/queries';
+import { LOGIN_USER } from '../utils/mutations';
 
 const Homepages = () => {
   const { loading, data } = useQuery(QUERY_ITEMS);
-  const items = data?.items || [];
+  const { data: userData } = useQuery(LOGIN_USER);
+  const thoughts = data?.thoughts || [];
 
   const loggedIn = Auth.loggedIn();
 
   return (
     <main>
-      {/* <div className="flex-row justify-space-between">
+      <div className="flex-row justify-space-between">
         {loggedIn && (
           <div className="col-12 mb-3">
-            <ThoughtForm />
+            <Profile/>
           </div>
         )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+            <QUERY_ITEMS thoughts={thoughts} title="Any good Barters?(s)..." />
           )}
         </div>
         {loggedIn && userData ? (
           <div className="col-12 col-lg-3 mb-3">
-            
+            <singleitems
+              username={userData.me.username}
+              friendCount={userData.me.friendCount}
+              friends={userData.me.friends}
+            />
           </div>
         ) : null}
-      </div> */}
-
+      </div>
     </main>
   );
 };
 
-export default Homepages;
+export default Home;
