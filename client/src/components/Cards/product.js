8e14a8ab540b170b7fbb5b146fefd78a74bd798b
Abigail Adams <abigail-adams@gmail.com>
2021-05-26 18:57:11 -0700

    adding responsiveness

--- client/src/components/Cards/product.js
+++ client/src/components/Cards/product.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { useTheme } from "@material-ui/styles";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -14,23 +13,11 @@ import { blue } from "@material-ui/core/colors";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 // import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import { Link } from "react-router-dom";
-import { GridList } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  //   root: {
-  //     maxWidth: 345,
-  //     [theme.breakpoints.down("md")]: {
-  //       maxWidth: 200,
-  //     },
-  //   },
-  //   media: {
-  //     height: 140,
-  //   },
-  // }));
   root: {
     background: "linear-gradient(45deg, #49ABCA 30%, #214381 90%)",
     maxWidth: 250,
@@ -90,12 +77,13 @@ export default function ItemCard({ item }) {
           </IconButton>
         }
         title={item.title}
-        subheader={item.createdAt}
+
+        // subheader={item.createdAt}
       />
 
       <CardMedia
         component={Link}
-        to={`/item/${item.id}`}
+        to={`/item/${item._id}`}
         className={classes.media}
         image={item.image}
         title={item.title}
@@ -130,23 +118,7 @@ export default function ItemCard({ item }) {
         </IconButton> */}
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then
-            serve.
-          </Typography>
-        </CardContent>
+        <CardContent></CardContent>
       </Collapse>
     </Card>
   );
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="w-100 bg-secondary p-3">
-      <div className="footer-container ">Copyright &copy; 2020 BARTERMART</div>
+      <div className="footer-container copyright ">
+        Copyright &copy; 2021 BARTERMART
+      </div>
       {/* <div className="customerSupport"></div> */}
     </footer>
   );
